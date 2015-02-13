a089c24ad4b7af8db9ffedabaef246a1d54f7cd1
OleKN <ok.nakken@gmail.com>
2015-02-13 14:46:39 +0100

    Fixed a bug where the real-time departure queries would use the busStop.ID instead of the lineName parameter

--- controllers/busLocationController.js
+++ controllers/busLocationController.js
@@ -44,27 +44,36 @@ exports.getRealTimeLineInfo = function(req, res){
 		// Ask for bus stop visits for every bus stop for a given line
 		for(var i = 0; i < busStopIDs.length; i++){
 			busStopModel.findOne({ID: busStopIDs[i], Operator: operatorParam} , function(err, busStop){
+				console.log(busStopVisitURL + busStop.ID + "?json=true&linenames=" + lineNameParam);
 				// Sends a request for all stops, to get real-time information about arrival times
-				request({
-					url: busStopVisitURL + busStop.ID + "?json=true&linenames=" + busStop.Name,
-					json: true
-					}, function(error, reponse, busStopVisitList){
-						var stopVisits = [];
-						// Store information about each arrival in a nice format
-						for(var x = 0; x < busStopVisitList.length; x++){
-							var newVisit = makeNewVisit(busStopVisitList[x]);
-							stopVisits.push(newVisit);
-						}
 
-						// Store information about each bus stop
-						var newStopVisitJSON = {
-							BusStopID: busStop.ID,
-							BusStopName: busStop.Name,
-							Position: busStop.Position,
-							StopVisits: stopVisits
+				request({
+					url: busStopVisitURL + busStop.ID + "?json=true&linenames=" + lineNameParam,
+					json: true
+					}, function(error, response, busStopVisitList){
+
+						if(!error && response.statusCode === 200){
+							console.log(busStopVisitList);
+							var stopVisits = [];
+							// Store information about each arrival in a nice format
+							for(var x = 0; x < busStopVisitList.length; x++){
+								var newVisit = makeNewVisit(busStopVisitList[x]);
+								stopVisits.push(newVisit);
+							}
+
+							// Store information about each bus stop
+							var newStopVisitJSON = {
+								BusStopID: busStop.ID,
+								BusStopName: busStop.Name,
+								Position: busStop.Position,
+								StopVisits: stopVisits
+							}
+							arrivalsList.push(newStopVisitJSON);
+							cb(arrivalsList);				
+						}else{
+							console.error(error);
+							console.log(response);
 						}
-						arrivalsList.push(newStopVisitJSON);
-						cb(arrivalsList);				
 					}
 				)
 			})
